eb21a4135e6ddcae8565a3e563d0cb0ffd00d456
Aryan Luthra <pbdl3846@leeds.ac.uk>
2024-10-27 16:04:16 +0000

    worksheet_4

---
+++ currency_converter.py
@@ -0,0 +1,24 @@
+
+def currency_converter(amount, from_currency, to_currency):
+    
+    conversion_rate = {
+    'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
+    'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
+    'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
+    'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
+    }
+
+    if amount < 0:
+        return 0.0
+
+    rate = conversion_rate[from_currency][to_currency]
+
+    converted_amount = amount * rate
+
+    return round(converted_amount, 2)
+
+#TEST
+#print(currency_converter(100,'GBP','INR'))
+
+# Submit only this file currency_converter.py with the completed function.
+# Do not add additional code for calling the function, or code to get user input.
